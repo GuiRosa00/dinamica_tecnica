7c15c4d66369f4f224a34a089d067b720ca8bce8
mario jorge rosa rosa <guilherme.rosa@poli.ufrj.br>
2022-01-17 00:58:13 -0300

    adicionado mais features em comandos

--- terminal_system/comandos.py
+++ terminal_system/comandos.py
@@ -1,26 +1,25 @@
-from terminal_system.classes import Cliente,Consultor
-from time import sleep
+from classes import Cliente,Consultor
 
+s=None
 contratos={}
 clientes = {}
 consultores = {}
 
 #Bloco de funções do Menu principal
-def criar_consultor(): #não adiciona no dic e nem cria consultor
+def post_consultor(): #não adiciona no dic e nem cria consultor
     username = input("Username\n")
     password = input("Password\n")
-    
     print("Consultor adicionado ao sistema!")
     return None
 
-def criar_cliente(): #não adiciona no dic e nem cria cliente
+def post_cliente(): #não adiciona no dic e nem cria cliente
     username = input("Username\n")
     password = input("Password\n")
     
     print("Consultor adicionado ao sistema!")
     return None
           
-def criar_Contrato():
+def post_contrato():
     title = input("Title\n")
     description = input("Description\n")
     end_period = input("End Period\n")
@@ -37,3 +36,26 @@ def criar_Contrato():
     contratos[f'{contrato.id}']=contrato
     print("Consultor adicionado ao sistema!") ### print errado
     return None
+
+def conclude_contrato():
+    try:contrato = contratos[input("Id do Contrato:\n")]
+    except KeyError:
+        print("Não existe contrato com esse id")
+    contrato.conclude_contract()
+
+def get_consultores():
+    print("Consultor Log")
+    for consultor in consultores.values():
+        consultor.view()
+    print("----------------------------")
+
+def get_consultor():
+    print("Consultor Log")
+    id = int(input("Id do consultor"))
+    consultores[id].view()
+    print("----------------------------")
+
+def delete_consultor():
+    username = input("Username\n")
+    password = input("Password\n")
+    print("----------------------------")
